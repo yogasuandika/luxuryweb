f7d4e67647df12e146f40b6448d91d5e029052c4
Yoga Bagus <email.gedeyoga@gmail.com>
2021-01-20 11:56:44 +0800

    deploy web

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: {
-    enabled: true,
+    //enabled: true,
     content: ["./**/*.html", "./**/*.js"],
   },
   presets: [],
@@ -65,6 +65,7 @@ module.exports = {
       64: "16rem",
       72: "18rem",
       80: "20rem",
+      88: "22rem",
       96: "24rem",
     },
     animation: {
@@ -174,38 +175,13 @@ module.exports = {
     },
     fontFamily: {
       sans: [
-        "ui-sans-serif",
-        "system-ui",
-        "-apple-system",
-        "BlinkMacSystemFont",
-        '"Segoe UI"',
-        "Roboto",
-        '"Helvetica Neue"',
-        "Arial",
-        '"Noto Sans"',
-        "sans-serif",
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
+        "'Open Sans', sans-serif;",
       ],
       serif: [
-        "ui-serif",
-        "Georgia",
-        "Cambria",
-        '"Times New Roman"',
-        "Times",
-        "serif",
+        
       ],
       mono: [
-        "ui-monospace",
-        "SFMono-Regular",
-        "Menlo",
-        "Monaco",
-        "Consolas",
-        '"Liberation Mono"',
-        '"Courier New"',
-        "monospace",
+        
       ],
     },
     fontSize: {
@@ -517,6 +493,7 @@ module.exports = {
       20: "0.2",
       25: "0.25",
       30: "0.3",
+      35: "0.35",
       40: "0.4",
       50: "0.5",
       60: "0.6",
